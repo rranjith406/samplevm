841219966fc6c3bd80a8687e31d4b33ecbc2adbb
rranjith406 <52668325+rranjith406@users.noreply.github.com>
2020-07-19 19:45:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,8 +5,19 @@ output "public_ip" {
 output "subnet_id"{
   value = "${aws_instance.this.subnet_id}"
 }
+
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+CONFIGMAPAWSAUTH
+  }
 resource "null_resource" "web" {
   provisioner "local-exec" {
     command = "echo ${aws_instance.this.private_ip} >> /home/ec2-user/private_ips.txt"
+    command ="echo ${local.config_map_aws_auth} >> /home/ec2-user/private_ips.txt"
   }
 }
