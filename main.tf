c91b7fe74f301e60fb2e15aaa20dd39e0611f42b
rranjith406 <52668325+rranjith406@users.noreply.github.com>
2020-07-19 18:14:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "ap-south-1"
+  version = ">= 2.38.0"
+  shared_credentials_file = "/home/ec2-user/cred"
+  profile = "default"
 }
+resource "aws_instance" "this" {
+  ami              = "ami-0732b62d310b80e97"
+  instance_type    = "t2.micro"
+  associate_public_ip_address = "true"
+  }
