7be43995057cebd679e1beecb86fca926e881f37
rranjith406 <52668325+rranjith406@users.noreply.github.com>
2020-07-19 19:46:48 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,7 +17,7 @@ CONFIGMAPAWSAUTH
   }
 resource "null_resource" "web" {
   provisioner "local-exec" {
-    command = "echo ${aws_instance.this.private_ip} >> /home/ec2-user/private_ips.txt"
+    //command = "echo ${aws_instance.this.private_ip} >> /home/ec2-user/private_ips.txt"
     command ="echo ${local.config_map_aws_auth} >> /home/ec2-user/private_ips.txt"
   }
 }
