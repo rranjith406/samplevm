aa7bfb4f1ed6bf90bb1a148838f4354e3e51f466
rranjith406 <52668325+rranjith406@users.noreply.github.com>
2020-07-19 18:32:40 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = "${aws_instance.this.*.public_ip}"
+  value       = "${aws_instance.this.public_ip}"
 }
 output "subnet_id"{
-  value = "${aws_instance.this.subnet_id
+  value = "${aws_instance.this.subnet_id}"
 }
