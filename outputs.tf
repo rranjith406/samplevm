55935f7a5cdded319f5d335d135b90b64c284038
rranjith406 <52668325+rranjith406@users.noreply.github.com>
2020-07-19 19:39:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "public_ip" {
 output "subnet_id"{
   value = "${aws_instance.this.subnet_id}"
 }
+resource "null_resource" "web" {
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.this.private_ip} >> /home/ec2-user/private_ips.txt"
+  }
+}
